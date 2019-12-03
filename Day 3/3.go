56c1e92f66d203d1b0820f4803c1a9cee9baa63a
unknown <tatekennington@gmail.com>
2019-12-03 18:44:28 +1300

    Tidied up the code

--- Day 3/3.go
+++ Day 3/3.go
@@ -6,94 +6,75 @@ import "strconv";
 import "os";
 import "strings";
 
+
 func main(){
 	var kb = bufio.NewScanner(os.Stdin);
-	kb.Scan();
-	var wire1 = kb.Text();
 	var points = make(map[[2]int]int);
 	var curr = [2]int{0,0};
+	var step = [2]int{0,0};
 	var count = 0;
+	var ans = -1;
+	var wire1 string;
+	var wire2 string;
+
+	kb.Scan();
+	wire1 = kb.Text();
+	kb.Scan();
+	wire2 = kb.Text();
+
 	for _, x := range strings.Split(wire1, ","){
-		var dir = x[0];
 		dist, _ := strconv.Atoi(x[1:]);
-		if dir == 'R'{
-			for i:=0; i<dist; i++{
-				count++;
-				curr[0]++;
-				points[curr] = count;
-			}
+
+		switch(x[0]){
+			case 'R':
+				step[0] = 1;
+				step[1] = 0;
+			case 'L':
+				step[0] = -1;
+				step[1] = 0;
+			case 'U':
+				step[0] = 0;
+				step[1] = 1;
+			case 'D':
+				step[0] = 0;
+				step[1] = -1;
 		}
-		if dir == 'L'{
-			for i:=0; i<dist; i++{
-				count++;
-				curr[0]--;
-				points[curr] = count;
-			}
-		}
-		if dir == 'U'{
-			for i:=0; i<dist; i++{
-				count++;
-				curr[1]++;
-				points[curr] = count;
-			}
-		}
-		if dir == 'D'{
-			for i:=0; i<dist; i++{
-				count++;
-				curr[1]--;
-				points[curr] = count;
-			}
+
+		for i:=0; i<dist; i++{
+			count++;
+			curr[0]+=step[0];
+			curr[1]+=step[1];
+			points[curr] = count;
 		}
 	}
-	kb.Scan();
-	var wire2 = kb.Text();
+
 	curr = [2]int{0,0};
-	var ans = -1;
 	count = 0;
 	for _, x := range strings.Split(wire2, ","){
-		var dir = x[0];
 		dist, _ := strconv.Atoi(x[1:]);
-		if dir == 'R'{
-			for i:=0; i<dist; i++{
-				count++;
-				curr[0]++;
-				if temp, ok := points[curr]; ok{
-					if ans == -1 || temp+count<ans{
-						ans = temp+count;
-					}
-				}
-			}
+
+		switch(x[0]){
+			case 'R':
+				step[0] = 1;
+				step[1] = 0;
+			case 'L':
+				step[0] = -1;
+				step[1] = 0;
+			case 'U':
+				step[0] = 0;
+				step[1] = 1;
+			case 'D':
+				step[0] = 0;
+				step[1] = -1;
 		}
-		if dir == 'L'{
-			for i:=0; i<dist; i++{
-				count++;
-				curr[0]--;
-				if temp, ok := points[curr]; ok{
-					if ans == -1 || temp+count<ans{
-						ans = temp+count;
-					}
-				}
-			}
-		}
-		if dir == 'U'{
-			for i:=0; i<dist; i++{
-				count++;
-				curr[1]++;
-				if temp, ok := points[curr]; ok{
-					if ans == -1 || temp+count<ans{
-						ans = temp+count;
-					}
-				}
-			}
-		}
-		if dir == 'D'{
-			for i:=0; i<dist; i++{
-				count++;
-				curr[1]--;
-				if temp, ok := points[curr]; ok{
-					if ans == -1 || temp+count<ans{
-						ans = temp+count;
-					}
+
+		for i:=0; i<dist; i++{
+			count++;
+			curr[0]+=step[0];
+			curr[1]+=step[1];
+			if temp, ok := points[curr]; ok{
+				if ans == -1 || temp + count < ans {
+					ans = temp+count;
 				}
 			}
 		}
